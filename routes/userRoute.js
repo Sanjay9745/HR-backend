6db695d01a057fac05cf179a90740c2423673d2f
Sanjay9745 <sanjaysanthosh919@gmail.com>
2023-08-15 08:33:48 +0530

    testing

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -62,7 +62,7 @@ router.get('/getIP', (req, res) => {
     res.status(400).json({ message: 'Error getting IP address.' });
   }
 });
-router.get('/getIp', (req, res) => {
+router.get('/getIp-ad', (req, res) => {
   const ipAddress = req.ip; // Get the IP address from the request
   res.json({ ip: ipAddress });
 });
