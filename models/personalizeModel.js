1c622eb328ec74a2f7f84359e54bd047d1dfdeb9
Sanjay9745 <sanjaysanthosh919@gmail.com>
2023-08-16 09:11:12 +0530

    till additional matrix added

--- models/personalizeModel.js
+++ models/personalizeModel.js
@@ -1,56 +1,95 @@
+const mongoose = require("mongoose");
+const personalizeSchema = new mongoose.Schema({
+  user_id: { type: String, required: true },
+  date: { type: Date, default: Date.now() },
+  //cultural
+  cultural: {
+    country: { type: String, default: "" },
+    number: { type: String, default: "" },
+    currency: { type: String, default: "" },
+    percentage: { type: String, default: "" },
+    date_format: { type: String, default: "" },
+    timezone: { type: String, default: "" },
+    percentage_round: { type: String, default: "" },
+    number_round: { type: String, default: "" },
+    currency_round: { type: String, default: "" },
+  },
+  //salary component
+  salary: {
+    salary_component: { type: Boolean, default: false },
+    merit_applied_on: { type: Object, default: {} },
+  },
+  //integration with hr
+  integration_with_hr: {
+    integration_with_hr: { type: Boolean, default: false },
+    integration_image: { type: Object, default: {} },
+  },
+  
+  //company profile
+  company: {
+    company_name: { type: String, default: "" },
+    company_address: { type: String, default: "" },
+    company_industry: { type: String, default: "" },
+    company_phone: { type: String, default: "" },
+    company_email: { type: String, default: "" },
+    company_profile: { type: String, default: "" },
+    company_approver_name: { type: String, default: "" },
+    company_approver_email: { type: String, default: "" },
+    company_approver_replay_email: { type: String, default: "" },
+    company_signature: { type: Object, default: {} },
+    company_logo: { type: Object, default: {} },
+    company_hr_logo: { type: Object, default: {} },
+  },
 
-const mongoose = require('mongoose');
-const personalizeSchema = new mongoose.Schema( {
-    user_id:  { type: String, required: true},
-    date: { type: Date, default: Date.now()},
-    //cultural
-    country :   { type: String, default: ""},
-    number :  { type: String, default: ""},
-    currency :  { type: String, default: ""},
-    percentage:  { type: String, default: ""},
-    date_format:  { type: String, default: ""},
-    timezone: { type: String, default: ""},
-    percentage_round:  { type: String, default: "" },
-    number_round:  { type: String, default: "" },
-    currency_round:  { type: String, default: ""},
-    //salary component
-    salary_component:  { type: Boolean, default: false},
-    merit_applied_on:  { type: Object, default: {}},
-    //integration with hr
-    integration_with_hr:  { type: Boolean, default: false},
-    integration_image:  { type: Object,default: {}},
-    //company profile
-    company_name:  { type: String, default: ""},
-    company_address:  { type: String, default: ""},
-    company_industry:  { type: String, default: ""},
-    company_phone:  { type: String, default: ""},
-    company_email:  { type: String, default: ""},
-    company_profile:  { type: String, default: ""},
-    company_approver_name:  { type: String, default: ""},
-    company_approver_email:  { type: String, default: ""},
-    company_approver_replay_email:  { type: String, default: ""},
-    company_signature:  { type: Object,default: {}},
-    company_logo:  { type: Object,default: {}},
-    company_hr_logo:  { type: Object,default: {}},
-    //workflow
-    workflow:  { type: Object, default: {}},
-    //exclusion criteria
-    exclusion_criteria:  { type: Object, default: {}},
-    //Supervisor
-    load_policy:   { type: Object, default: {}},
-    load_learning_material:   { type: Object, default: {}},
-    supervisor_video_url:   { type: String, default: ""},
-    supervisor_attachment:   { type: Boolean, default: false},
-    supervisor_currency_button:   { type: Boolean, default: false},
-    supervisor_enable_chat:   { type: Boolean, default: false},
-    supervisor_employee_letter:   { type: Boolean, default: false},
-    //hr review settings
-    allow_hr_override:   { type: Boolean, default: false},
-    justification_mandatory:   { type: Boolean, default: false},
-    attachment_mandatory:   { type: Boolean, default: false},
-    edit_on_approval:   { type: Boolean, default: false},
+  //workflow
+  workflow: { type: Object, default: {} },
+  //exclusion criteria
+  exclusion_criteria: { type: Object, default: {} },
+  //Supervisor
+  supervisor: {
+    load_policy: { type: Object, default: {} },
+    load_learning_material: { type: Object, default: {} },
+    supervisor_video_url: { type: String, default: "" },
+    supervisor_attachment: { type: Boolean, default: false },
+    supervisor_currency_button: { type: Boolean, default: false },
+    supervisor_enable_chat: { type: Boolean, default: false },
+    supervisor_employee_letter: { type: Boolean, default: false },
+  },
 
+  //hr review settings
+  hr_review: {
+    allow_hr_override: { type: Boolean, default: false },
+    justification_mandatory: { type: Boolean, default: false },
+    attachment_mandatory: { type: Boolean, default: false },
+    edit_on_approval: { type: Boolean, default: false },
+  },
+  //Total rewards Statement
+  total_rewards: {
+    total_cash: { type: Object, default: {} },
+    total_benefits: { type: Object, default: {} },
+    additional_analysis: { type: Object, default: {} },
+  },
+  //TAT
+    tat: {type: Number, default: 0},
+   //Terminology
+    terminology: {
+        my_team: {type: String, default: ""},
+        salary: {type: String, default: ""},
+        compa_ratio: {type: String, default: ""},
+        range_penetration: {type: String, default: ""},
+        guideline: {type: String, default: ""},
+        recommendation: {type: String, default: ""},
+        new_salary: {type: String, default: ""},
+        median_salary: {type: String, default: ""},
+        medium_salary: {type: String, default: ""},
+    },
+    //Addition Matrix
+     addition_matrix: {
+        addition_matrix: {type: Boolean, default: false},
+        matrix_name: {type: String, default: ""},
+        factor_one: {type: String, default: ""},
+        factor_two: {type: String, default: ""},
+     }
 });
 
-
-module.exports = mongoose.model('Personalize',personalizeSchema);
+module.exports = mongoose.model("Personalize", personalizeSchema);
--- routes/personalizeRoute.js
+++ routes/personalizeRoute.js
@@ -19,7 +19,10 @@ const storage = multer.diskStorage({
 });
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith("image/") || file.mimetype.startsWith("application/pdf")) {
+  if (
+    file.mimetype.startsWith("image/") ||
+    file.mimetype.startsWith("application/pdf")
+  ) {
     cb(null, true);
   } else {
     cb(new Error("Unsupported file type"), false);
@@ -33,8 +36,6 @@ const upload = multer({
   },
   fileFilter: fileFilter,
 });
-
-
 
 //cultural
 router.post("/cultural", authenticateToken, async (req, res) => {
@@ -74,15 +75,17 @@ router.post("/cultural", authenticateToken, async (req, res) => {
     let personalizeRecord = await Personalize.findOneAndUpdate(
       { user_id: userId },
       {
-        country,
-        number,
-        currency,
-        percentage,
-        date_format,
-        timezone,
-        percentage_round,
-        number_round,
-        currency_round,
+        cultural: {
+          country,
+          number,
+          currency,
+          percentage,
+          date_format,
+          timezone,
+          percentage_round,
+          number_round,
+          currency_round,
+        },
       },
       { new: true, upsert: true }
     );
@@ -102,15 +105,19 @@ router.post("/salary-component", authenticateToken, async (req, res) => {
 
     // Check if the required fields are not null
     if (!salary_component || !merit_applied_on) {
-      return res.status(400).json({ error: "salary_component and merit_applied_on are required fields." });
+      return res.status(400).json({
+        error: "salary_component and merit_applied_on are required fields.",
+      });
     }
 
     // Create or update the Personalize record
     let personalizeRecord = await Personalize.findOneAndUpdate(
       { user_id: userId },
       {
-        salary_component,
-        merit_applied_on,
+        salary: {
+          salary_component,
+          merit_applied_on,
+        },
       },
       { new: true, upsert: true, useFindAndModify: false }
     );
@@ -123,38 +130,45 @@ router.post("/salary-component", authenticateToken, async (req, res) => {
 });
 
 //integration with hr
-router.post("/integration-with-hr", authenticateToken, upload.fields([
-  { name: "integration_image", maxCount: 1 },
-]), async (req, res) => {
-  try {
-    const { integration_with_hr } = req.body;
-    const IntegrationWithHRFile = req.files["integration_image"][0];
-    const userId = req.user._id;
+router.post(
+  "/integration-with-hr",
+  authenticateToken,
+  upload.fields([{ name: "integration_image", maxCount: 1 }]),
+  async (req, res) => {
+    try {
+      const { integration_with_hr } = req.body;
+      const IntegrationWithHRFile = req.files["integration_image"][0];
+      const userId = req.user._id;
 
-    // Check if the integration_with_hr field is not null
-    if (!integration_with_hr) {
-      return res.status(400).json({ error: "integration_with_hr is a required field." });
+      // Check if the integration_with_hr field is not null
+      if (!integration_with_hr) {
+        return res
+          .status(400)
+          .json({ error: "integration_with_hr is a required field." });
+      }
+
+      // Assuming that you have a Personalize schema
+      let personalizeRecord = await Personalize.findOneAndUpdate(
+        { user_id: userId },
+        {
+          integration_with_hr: {
+            integration_with_hr,
+            // Assuming you want to store the image URL in the personalizeRecord
+            integration_image: IntegrationWithHRFile
+              ? IntegrationWithHRFile
+              : null,
+          },
+        },
+        { new: true, upsert: true, useFindAndModify: false }
+      );
+
+      res.json(personalizeRecord);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send();
     }
-
-    // Assuming that you have a Personalize schema
-    let personalizeRecord = await Personalize.findOneAndUpdate(
-      { user_id: userId },
-      {
-        integration_with_hr,
-        // Assuming you want to store the image URL in the personalizeRecord
-        integration_image: IntegrationWithHRFile ? IntegrationWithHRFile : null,
-      },
-      { new: true, upsert: true, useFindAndModify: false }
-    );
-
-    res.json(personalizeRecord);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send();
   }
-});
-
-
+);
 
 //company profile
 router.post(
@@ -209,32 +223,22 @@ router.post(
       let personalizeRecord = await Personalize.findOneAndUpdate(
         { user_id: userId },
         {
-          company_name,
-          company_address,
-          company_industry,
-          company_phone,
-          company_email,
-          company_profile,
-          company_approver_name,
-          company_approver_email,
-          company_approver_replay_email,
-          company_signature: {
-            filename: companySignatureFile.originalname,
-            contentType: companySignatureFile.mimetype,
-            data: companySignatureFile.buffer,
+          company: {
+            company_name,
+            company_address,
+            company_industry,
+            company_phone,
+            company_email,
+            company_profile,
+            company_approver_name,
+            company_approver_email,
+            company_approver_replay_email,
+            company_signature: companySignatureFile
+              ? companySignatureFile
+              : null,
+            company_logo: companyLogoFile ? companyLogoFile : null,
+            company_hr_logo: companyHrLogoFile ? companyHrLogoFile : null,
           },
-          company_logo: {
-            filename: companyLogoFile.originalname,
-            contentType: companyLogoFile.mimetype,
-            data: companyLogoFile.buffer,
-          },
-          company_hr_logo: companyHrLogoFile
-            ? {
-                filename: companyHrLogoFile.originalname,
-                contentType: companyHrLogoFile.mimetype,
-                data: companyHrLogoFile.buffer,
-              }
-            : null,
         },
         { new: true, upsert: true }
       );
@@ -247,5 +251,248 @@ router.post(
   }
 );
 //workflow
+router.post("/workflow", authenticateToken, async (req, res) => {
+  try {
+    const { workflow } = req.body;
 
+    // Check if the workflow field is present in the request body
+    if (!workflow) {
+      return res.status(400).json({ error: "workflow is a required field." });
+    }
+
+    const userId = req.user._id;
+    
+    // Assuming that you have a Personalize schema
+    let personalizeRecord = await Personalize.findOneAndUpdate(
+      { user_id: userId },
+      {
+        workflow,
+      },
+      { new: true, upsert: true, useFindAndModify: false }
+    );
+
+    res.json(personalizeRecord);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
+
+//exclusion criteria
+router.post("/exclusion-criteria", authenticateToken, async (req, res) => {
+  try {
+    const { exclusion_criteria } = req.body;
+
+    // Check if the exclusion_criteria field is present in the request body
+    if (!exclusion_criteria) {
+      return res.status(400).json({ error: "exclusion_criteria is a required field." });
+    }
+
+    const userId = req.user._id;
+    
+    // Assuming that you have a Personalize schema
+    let personalizeRecord = await Personalize.findOneAndUpdate(
+      { user_id: userId },
+      {
+        exclusion_criteria,
+      },
+      { new: true, upsert: true, useFindAndModify: false }
+    );
+
+    res.json(personalizeRecord);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
+
+
+//Supervisor
+
+router.post(
+  "/supervisor",
+  authenticateToken,
+  upload.fields([
+    { name: "load_policy", maxCount: 1 },
+    { name: "load_learning_material", maxCount: 1 },
+  ]),
+  async (req, res) => {
+    try {
+      const {
+        supervisor_video_url,
+        supervisor_attachment,
+        supervisor_currency_button,
+        supervisor_enable_chat,
+        supervisor_employee_letter,
+      } = req.body;
+
+      // Check if the supervisor_video_url field is not null
+      if (!supervisor_video_url) {
+        return res
+          .status(400)
+          .json({ error: "supervisor_video_url is a required field." });
+      }
+
+      // Check if the uploaded files are present
+      const load_policy = req.files["load_policy"] ? req.files["load_policy"][0] : null;
+      const load_learning_material = req.files["load_learning_material"] ? req.files["load_learning_material"][0] : null;
+
+      const userId = req.user._id;
+
+      // Assuming that you have a Personalize schema
+      let personalizeRecord = await Personalize.findOneAndUpdate(
+        { user_id: userId },
+        {
+          supervisor: {
+            supervisor_video_url,
+            supervisor_attachment,
+            supervisor_currency_button,
+            supervisor_enable_chat,
+            supervisor_employee_letter,
+            load_policy: load_policy ? load_policy.path : null,
+            load_learning_material: load_learning_material
+              ? load_learning_material.path
+              : null,
+          },
+        },
+        { new: true, upsert: true, useFindAndModify: false }
+      );
+
+      res.json(personalizeRecord);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send();
+    }
+  }
+);
+
+//hr review settings
+router.post("/hr-review-settings", authenticateToken, async (req, res) => {
+  try {
+    const {
+      allow_hr_override,
+      justification_mandatory,
+      attachment_mandatory,
+      edit_on_approval,
+    } = req.body;
+    const userId = req.user._id;
+    // Assuming that you have a Personalize schema
+    let personalizeRecord = await Personalize.findOneAndUpdate(
+      { user_id: userId },
+      {
+        hr_review: {
+          allow_hr_override,
+          justification_mandatory,
+          attachment_mandatory,
+          edit_on_approval,
+        },
+      },
+      { new: true, upsert: true, useFindAndModify: false }
+    );
+
+    res.json(personalizeRecord);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
+
+//total rewards
+router.post("/total-rewards", authenticateToken, async (req, res) => {
+  try {
+    const { total_cash, total_benefits, additional_analysis } = req.body;
+    const userId = req.user._id;
+    let personalizeRecord = await Personalize.findOneAndUpdate(
+      { user_id: userId },
+      {
+        total_rewards: { total_cash, total_benefits, additional_analysis },
+      },
+      { new: true, upsert: true, useFindAndModify: false }
+    );
+    res.json(personalizeRecord);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
+
+//TAT
+router.post("/tat", authenticateToken, async (req, res) => {
+  try {
+    const { tat } = req.body;
+    const userId = req.user._id;
+    let personalizeRecord = await Personalize.findOneAndUpdate(
+      { user_id: userId },
+      {
+        tat,
+      },
+      { new: true, upsert: true, useFindAndModify: false }
+    );
+    res.json(personalizeRecord);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
+
+//Terminology
+router.post("/terminology", authenticateToken, async (req, res) => {
+  try {
+    const {
+      my_team,
+      salary,
+      compa_ratio,
+      range_penetration,
+      guideline,
+      recommendation,
+      new_salary,
+      median_salary,
+      medium_salary,
+    } = req.body;
+    const userId = req.user._id;
+    let personalizeRecord = await Personalize.findOneAndUpdate(
+      { user_id: userId },
+      {
+        terminology: {
+          my_team,
+          salary,
+          compa_ratio,
+          range_penetration,
+          guideline,
+          recommendation,
+          new_salary,
+          median_salary,
+          medium_salary,
+        },
+      },
+      { new: true, upsert: true, useFindAndModify: false }
+    );
+    res.json(personalizeRecord);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
+
+// addition_matrix
+router.post("/addition-matrix", authenticateToken, async (req, res) => {
+  try {
+    const { addition_matrix, matrix_name, factor_one, factor_two } = req.body;
+    const userId = req.user._id;
+    let personalizeRecord = await Personalize.findOneAndUpdate(
+      { user_id: userId },
+      {
+        addition_matrix,
+        matrix_name,
+        factor_one,
+        factor_two,
+      },
+      { new: true, upsert: true, useFindAndModify: false }
+    );
+    res.json(personalizeRecord);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
 module.exports = router;
