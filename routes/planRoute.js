4163be4ddde8ce669591cd1182402b9e0a72000f
Sanjay9745 <sanjaysanthosh919@gmail.com>
2023-09-11 20:14:01 +0530

    pdf added not completed

--- routes/planRoute.js
+++ routes/planRoute.js
@@ -5,7 +5,10 @@ const authenticateToken = require("../middleware/auth");
 const multer = require("multer"); // Import Multer
 const path = require("path");
 const fs = require("fs");
+const puppeteer = require("puppeteer");
 const XLSX = require("xlsx");
+const {sendBulkMail} = require("../controllers/emailController");
+const Personalize = require("../models/personalizeModel");
 // Configure Multer storage
 // const storage = multer.diskStorage({
 //     destination: function (req, file, cb) {
@@ -40,8 +43,30 @@ const XLSX = require("xlsx");
 //     },
 //     fileFilter: fileFilter,
 //   });
+
+//bulk mail
+router.get("/send-mail", async (req, res) => {
+  const recipients = ['sanjaysanthosh919@gmail.com', 'santhoshkvsanjay@example.com',"allissmallmalayalam@gmail.com"];
+  const subject = 'Your Email Subject';
+  const text = 'Your plain text email content';
+  const html = '<p>Your HTML email content</p>';
+  
+  sendBulkMail(recipients, subject, text, html)
+    .then((results) => {
+      console.log('Emails sent:', results);
+    })
+    .catch((error) => {
+      console.error('Error sending emails:', error);
+    });
+})
+
+
+
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
+
+
+
 router.get("/", authenticateToken, async (req, res) => {
   try {
     const plan = await Plan.findOne({ user_id: req.user._id });
@@ -52,6 +77,9 @@ router.get("/", authenticateToken, async (req, res) => {
     });
   }
 });
+
+
+
 router.post("/", authenticateToken, async (req, res) => {
   try {
     const planData = {
@@ -261,4 +289,189 @@ router.get("/template-file", authenticateToken, async (req, res) => {
     res.status(500).json({ error: "An error occurred" });
   }
 });
+
+//download pdf
+
+router.get('/download-pdf',authenticateToken, async (req,res)=>{
+   // Launch a headless Chrome browser
+   const userId = req.user._id;
+   const personalize = await Personalize.findOne({user_id:userId})
+   const plan = await Plan.findOne({user_id:userId})
+  console.log(personalize,plan);
+   const browser = await puppeteer.launch();
+
+   // Create a new page
+   const page = await browser.newPage();
+
+   // Set the HTML content for the PDF
+   const htmlContent = `
+   <html>
+   <head>
+     <title>Document</title>
+   </head>
+   <body style="margin:20px;">
+ 
+     <h2 style="color:grey;margin-top:50px;font-family: Tahoma;">Merit Compensation 2021-22</h2>
+     <hr />
+     <h3  style="color:dodgerblue;margin-top:40px;font-family: Tahoma;">Culture setting</h3>
+     <h4  style="color:grey;font-family: Tahoma;font-weight: normal;margin: 0;margin-bottom: 5px;">Base currency</h4>
+     <p style="font-family: Tahoma;font-weight: normal;font-size: 15px;margin: 0;">${personalize.cultural.currency}</p>
+     <h4  style="color:grey;font-family: Tahoma;font-weight: normal;margin-bottom: 5px;">Date format</h4>
+     <p style="font-family: Tahoma;font-weight: normal;font-size: 15px;margin: 0;margin-bottom: 5px;">${personalize.cultural.date_format}</p>
+     <h4 style="color:grey;font-family: Tahoma;font-weight: normal;margin: 0;margin-bottom: 5px;">Time Zone</h4>
+     <p style="font-family: Tahoma;font-weight: normal;font-size: 15px;">${personalize.cultural.timezone}</p>
+     <h3  style="color:dodgerblue;margin-top:40px;font-family: Tahoma;">Rounding rules</h3>
+     <h4  style="color:grey;font-family: Tahoma;font-weight: normal;margin: 0;margin-bottom: 5px;">Number</h4>
+     <p style="font-family: Tahoma;font-weight: normal;font-size: 15px;margin-top: 5px;">${personalize.cultural.number_round}</p>
+     <h4  style="color:grey;font-family: Tahoma;font-weight: normal;margin: 0;margin-bottom: 5px;">Percentage</h4>
+     <p style="font-family: Tahoma;font-weight: normal;font-size: 15px;">${personalize.cultural.percentage_round}</p>
+     <!-- <p ><span>Merit</span> <span>Promotion</span> <span>Bonus</span></p> -->
+     <h4 style="color:grey;font-family: Tahoma;font-weight: normal;margin: 0;margin-bottom: 5px;">Currency</h4>
+     <p style="font-family: Tahoma;font-weight: normal;font-size: 15px;">${personalize.cultural.currency_round}</p>
+ 
+         <hr>
+         <h3  style="color:dodgerblue;margin-top:40px;font-family: Tahoma;" >Multi currency</h3>
+         <h4  style="color:grey;font-family: Tahoma;font-weight: normal;margin: 0;margin-bottom: 5px;">Multi currency</h4>
+         <p style="font-family: Tahoma;font-weight: normal;font-size: 15px;">${personalize.salary.salary_component === true ? 'yes':'no'}</p>
+ 
+         <hr>
+ 
+         <h3  style="color:dodgerblue;margin-top:40px;font-family: Tahoma;">Merit cycle & Eligibility</h3>
+         <h4  style="color:grey;font-family: Tahoma;font-weight: normal;margin: 0;margin-bottom: 5px;">Merit type</h4>
+         <p style="font-family: Tahoma;font-weight: normal;font-size: 15px;margin-top: 5px;">${plan.cycle_type}</p>
+ 
+         <h4  style="color:grey;font-family: Tahoma;font-weight: normal;margin: 0;margin-bottom: 5px;">From Period</h4>
+         <p style="font-family: Tahoma;font-weight: normal;font-size: 15px;margin-top: 5px;">${plan.cycle_from}</p>
+         <h4  style="color:grey;font-family: Tahoma;font-weight: normal;margin: 0;margin-bottom: 5px;">To Period</h4>
+         <p style="font-family: Tahoma;font-weight: normal;font-size: 15px;margin-top: 5px;">${plan.cycle_to}</p>
+ 
+         <h4  style="color:grey;font-family: Tahoma;font-weight: normal;margin: 0;margin-bottom: 5px;">Eligibility</h4>
+         <p style="font-family: Tahoma;font-weight: normal;font-size: 15px;margin-top: 5px;">Joined ${plan.eligibility_date}</p>
+ 
+         <hr>
+ 
+         <h3  style="color:dodgerblue;margin-top:40px;font-family: Tahoma;">Pro-rata increment to service</h3>
+         <h4  style="color:grey;font-family: Tahoma;font-weight: normal;margin: 0;margin-bottom: 5px;">Pro-rata increment to service</h4>
+         <p style="font-family: Tahoma;font-weight: normal;font-size: 15px;margin-top: 5px;">${plan.prorate === true ? 'yes':'no'}</p>
+ 
+         <h4  style="color:grey;font-family: Tahoma;font-weight: normal;margin: 0;margin-bottom: 5px;">Pro-rata increment to service Unit</h4>
+         <p style="font-family: Tahoma;font-weight: normal;font-size: 15px;margin-top: 5px;">${plan.off_cycle_prorate_unit}</p>
+         <h3  style="color:dodgerblue;margin-top:40px;font-family: Tahoma;">Pro-rata increment to service</h3>
+         <h4  style="color:grey;font-family: Tahoma;font-weight: normal;margin: 0;margin-bottom: 5px;">Pro-rata off cycle increment to service</h4>
+         <p style="font-family: Tahoma;font-weight: normal;font-size: 15px;margin-top: 5px;">${plan.off_cycle_prorate=== true ? 'yes':'no'}</p>
+ 
+        <hr>
+ 
+        
+        <h3  style="color:dodgerblue;margin-top:40px;font-family: Tahoma;">Split Appraisal & matrix manager</h3>
+        <h4  style="color:grey;font-family: Tahoma;font-weight: normal;margin: 0;margin-bottom: 5px;">Enable split appraisals
+       </h4>
+        <p style="font-family: Tahoma;font-weight: normal;font-size: 15px;margin-top: 5px;">${plan.split_recommendation=== true ? 'yes':'no'}</p>
+ 
+        <h4  style="color:grey;font-family: Tahoma;font-weight: normal;margin: 0;margin-bottom: 5px;">Enable matrix recommendations</h4>
+        <p style="font-family: Tahoma;font-weight: normal;font-size: 15px;margin-top: 5px;">${plan.matrix_recommendation=== true ? 'yes':'no'}</p>
+       <hr>
+        
+       <h3  style="color:dodgerblue;margin-top:40px;font-family: Tahoma;">Pay Groups</h3>
+       <h4  style="color:grey;font-family: Tahoma;font-weight: normal;margin: 0;margin-bottom: 5px;">Diffrent merit rule for different employee groups
+      </h4>
+       <p style="font-family: Tahoma;font-weight: normal;font-size: 15px;margin-top: 5px;">${plan.paygroups === true ? 'yes':'no'}</p>
+ 
+      <hr>
+ 
+      
+      <h3  style="color:dodgerblue;margin-top:40px;font-family: Tahoma;">Parity Measure</h3>
+      <h4  style="color:grey;font-family: Tahoma;font-weight: normal;margin: 0;margin-bottom: 5px;">Preferred parity measure</h4>
+      <p style="font-family: Tahoma;font-weight: normal;font-size: 15px;margin-top: 5px;">${plan.parity}</p>
+      <h3  style="color:dodgerblue;margin-top:40px;font-family: Tahoma;">Merit Guidelines</h3>
+      <h4  style="color:grey;font-family: Tahoma;font-weight: normal;margin: 0;margin-bottom: 5px;">Enable merit guidelines</h4>
+      <p style="font-family: Tahoma;font-weight: normal;font-size: 15px;margin-top: 5px;">${plan.enable_recommendation === true ? 'yes':'no'}</p>
+     
+      <h4  style="color:grey;font-family: Tahoma;font-weight: normal;margin: 0;margin-bottom: 5px;">Validation for supervisor Recommendation</h4>
+      <p style="font-family: Tahoma;font-weight: normal;font-size: 15px;margin-top: 5px;">Recommendation is allowed within the guidelines only</p>
+ 
+      <hr>
+      <h3  style="color:dodgerblue;margin-top:20px;font-family: Tahoma;">Lump Sum</h3>
+      <h4  style="color:grey;font-family: Tahoma;font-weight: normal;margin: 0;margin-bottom: 5px;">Employee salary goes above the pay range max</h4>
+      <p style="font-family: Tahoma;font-weight: normal;font-size: 15px;margin-top: 5px;">Ignore range min, and provide increment on current salary</p>
+      <h3  style="color:dodgerblue;margin-top:100px;font-family: Tahoma;padding-top:30px;">Corrections</h3>
+      <h4  style="color:grey;font-family: Tahoma;font-weight: normal;margin: 0;margin-bottom: 5px;">When employee salary is below the pay range min</h4>
+      <p style="font-family: Tahoma;font-weight: normal;font-size: 15px;margin-top: 5px;">Ignore range max, and provide increment</p>
+     
+ <hr>
+ 
+ <h3  style="color:dodgerblue;margin-top:40px;font-family: Tahoma;">Promotions</h3>
+ <h4  style="color:grey;font-family: Tahoma;font-weight: normal;margin: 0;margin-bottom: 5px;">Enable promotion recommendations</h4>
+ <p style="font-family: Tahoma;font-weight: normal;font-size: 15px;margin-top: 5px;">${plan.enable_recommendation === true ? 'yes':'no'}</p>
+ 
+ <hr>
+ <h3  style="color:dodgerblue;margin-top:40px;font-family: Tahoma;">Bonus and Incentives</h3>
+      <h4  style="color:grey;font-family: Tahoma;font-weight: normal;margin: 0;margin-bottom: 5px;">Enable bonus and incentives</h4>
+      <p style="font-family: Tahoma;font-weight: normal;font-size: 15px;margin-top: 5px;">${plan.enable_bonus_incentives === true ? 'yes':'no'}</p>
+      
+      <h4  style="color:grey;font-family: Tahoma;font-weight: normal;margin: 0;margin-bottom: 5px;">Different Bonus and incentives rules for different employee group</h4>
+      <p style="font-family: Tahoma;font-weight: normal;font-size: 15px;margin-top: 5px;">${plan.statutory_increment === true ? 'yes':'no'}</p>
+      <hr>
+      <h3 style="color:dodgerblue;margin-top:40px;font-family: Tahoma;">Bonus Table</h3>
+      <table style="margin-top: 1.5rem;margin-bottom: 1.5rem; width: 95%;border-collapse: collapse;">
+ 
+ <thead>
+  
+ <th style="border: 1px solid black;padding: 8px;text-align: center;font-family: Tahoma;">SI NO</th>
+ <th style="border: 1px solid black;padding: 8px;text-align: center;font-family: Tahoma;">Name</th>
+ <th style="border: 1px solid black;padding: 8px;text-align: center;font-family: Tahoma;">Type</th>
+ <th style="border: 1px solid black;padding: 8px;text-align: center;font-family: Tahoma;">Eligibility</th>
+ <th style="border: 1px solid black;padding: 8px;text-align: center;font-family: Tahoma;">Bonus(%)</th>
+ <th style="border: 1px solid black;padding: 8px;text-align: center;font-family: Tahoma;">Bonus Multiplier</th>
+ <th style="border: 1px solid black;padding: 8px;text-align: center;font-family: Tahoma;">Recommendation</th>
+ <th style="border: 1px solid black;padding: 8px;text-align: center;font-family: Tahoma;">Prorate</th>
+ </thead>
+ <tbody>
+  
+   <tr>
+     <td style="border: 1px solid black;padding: 8px;text-align: center;">1</td>
+     <td style="border: 1px solid black;padding: 8px;text-align: center;">Reteption	</td>
+     <td style="border: 1px solid black;padding: 8px;text-align: center;">Time</td>
+     <td style="border: 1px solid black;padding: 8px;text-align: center;">30-11-2016</td>
+     <td style="border: 1px solid black;padding: 8px;text-align: center;">10 %</td>
+     <td style="border: 1px solid black;padding: 8px;text-align: center;">No</td>
+     <td style="border: 1px solid black;padding: 8px;text-align: center;">Yes (25-75)</td>
+     <td style="border: 1px solid black;padding: 8px;text-align: center;">No</td>
+   
+   </tr>
+     <tr>
+     <td style="border: 1px solid black;padding: 8px;text-align: center;">2</td>
+     <td style="border: 1px solid black;padding: 8px;text-align: center;">Reteption	</td>
+     <td style="border: 1px solid black;padding: 8px;text-align: center;">Time</td>
+     <td style="border: 1px solid black;padding: 8px;text-align: center;">30-11-2016</td>
+     <td style="border: 1px solid black;padding: 8px;text-align: center;">10 %</td>
+     <td style="border: 1px solid black;padding: 8px;text-align: center;">No</td>
+     <td style="border: 1px solid black;padding: 8px;text-align: center;">Yes (25-75)</td>
+     <td style="border: 1px solid black;padding: 8px;text-align: center;">No</td>
+     </tr>
+ </tbody>
+      </table>
+ 
+   </body>
+ </html>
+   `;
+ 
+   // Navigate to a data URL with the HTML content
+   await page.goto(`data:text/html,${htmlContent}`);
+ 
+   // Generate the PDF with multiple pages
+   const pdfBuffer = await page.pdf({ format: "A4", printBackground: true });
+ 
+   // Close the browser
+   await browser.close();
+ 
+   // Set the response headers to indicate a PDF file
+   res.setHeader("Content-Type", "application/pdf");
+   res.setHeader("Content-Disposition", "attachment; filename=output.pdf");
+ 
+   // Send the PDF content to the client
+   res.send(pdfBuffer);
+ 
+   console.log("PDF with three A4-sized pages sent to the client.");
+})
 module.exports = router;
